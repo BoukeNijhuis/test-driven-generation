f23c5b4c3abc528dd167a84d14a8d4841ecff768
Bouke Nijhuis <bouke.nijhuis@gmail.com>
2024-03-20 18:37:53 +0100

    enables running as a jar file

--- src/test/java/nl/boukenijhuis/UtilsTest.java
+++ src/test/java/nl/boukenijhuis/UtilsTest.java
@@ -14,7 +14,7 @@ class UtilsTest {
         test("src/test/org/example/UtilsTest.java", "src/main/org/example");
         test("src/test/UtilsTest.java", "src/main");
         test("/Users/boukenijhuis/git/test-driven-generation-examples/src/test/java/org/example/oddeven/OddEvenTest.java",
-                "/Users/boukenijhuis/git/test-driven-generation-examples/src/main/java/org/example/oddeven/OddEvenTest.java");
+                "/Users/boukenijhuis/git/test-driven-generation-examples/src/main/java/org/example/oddeven");
     }
 
     private void test(String input, String output) {
